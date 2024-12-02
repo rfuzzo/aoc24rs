dfdb1f52375238135be4cd170fd1f36638b36a73
Moritz Baron <r.fuzzo@gmail.com>
2024-12-02 16:31:56 +0100

    day2 part 2

--- src/lib.rs
+++ src/lib.rs
@@ -42,64 +42,92 @@ pub fn day1() {
 }
 
 pub fn day2() {
-    use std::collections::VecDeque;
-
     let input = include_str!("../input/day2.txt");
 
     let mut safe = 0;
 
     for (i, line) in input.lines().enumerate() {
         // split line by whitespace
-        let mut numbers: VecDeque<i32> = line
+        let numbers: Vec<i32> = line
             .split_whitespace()
             .map(|x| x.parse().unwrap())
             .collect();
 
         // debug stuff
         let mut reason = "".to_owned();
-        let mut sequence = 0;
 
-        // check if safe
-        let mut safe_line = true;
-        let mut last_number = numbers.pop_front().unwrap();
-        let mut increasing = None;
+        let error_index = check_line(numbers.clone(), None, &mut reason);
 
-        for (j, n) in numbers.into_iter().enumerate() {
-            sequence = j + 2;
+        if let Some(ind) = error_index {
+            println!("#{};{} ({}): {}", i, ind, line, reason);
 
-            // check difference to last number
-            let diff = n - last_number;
-            if diff == 0 {
-                safe_line = false;
-                reason = "zero difference".to_owned();
-                break;
+            // for until numbers size
+            let mut found = false;
+            for j in 0..numbers.len() {
+                if check_line(numbers.clone(), Some(j), &mut reason).is_none() {
+                    found = true;
+                    break;
+                }
             }
 
-            // check if always either increasing or decreasing
-            if increasing.is_none() {
-                increasing = Some(diff > 0);
-            } else if increasing.unwrap() != (diff > 0) {
-                safe_line = false;
-                reason = "not always increasing or decreasing".to_owned();
-                break;
+            if !found {
+                println!("\tno safe line found");
+            } else {
+                safe += 1;
             }
-
-            // check if difference is at least one and at most three.
-            if diff.abs() > 3 || diff.abs() < 1 {
-                safe_line = false;
-                reason = "difference not between 1 and 3".to_owned();
-                break;
-            }
-
-            last_number = n;
-        }
-
-        if safe_line {
-            safe += 1;
         } else {
-            println!("#{};{} ({}) unsafe: {}", i, sequence, line, reason);
+            safe += 1;
         }
     }
 
     println!("{}", safe);
+}
+
+fn check_line(
+    mut numbers: Vec<i32>,
+    ignore_index: Option<usize>,
+    reason: &mut String,
+) -> Option<usize> {
+    if let Some(ignored) = ignore_index {
+        println!("ignoring index {}", ignored);
+
+        // remove from numbers
+        numbers.remove(ignored);
+    }
+
+    // check if safe
+    let mut last_number = numbers.first().unwrap();
+    let mut increasing = None;
+
+    for (j, n) in numbers.iter().enumerate() {
+        // ignore first
+        if j == 0 {
+            continue;
+        }
+
+        // check difference to last number
+        let diff = n - last_number;
+        if diff == 0 {
+            *reason = "zero difference".to_owned();
+            return Some(j);
+        }
+
+        // check if always either increasing or decreasing
+        if increasing.is_none() {
+            increasing = Some(diff > 0);
+        } else if increasing.unwrap() != (diff > 0) {
+            *reason = "not always increasing or decreasing".to_owned();
+            return Some(j);
+        }
+
+        // check if difference is at least one and at most three.
+        if diff.abs() > 3 || diff.abs() < 1 {
+            *reason = "difference not between 1 and 3".to_owned();
+            return Some(j);
+        }
+
+        last_number = n;
+    }
+
+    None
 }
