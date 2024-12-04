7181368f7d482b0aa00b91c7d04b869704d33fd3
Moritz Baron <r.fuzzo@gmail.com>
2024-12-04 13:35:18 +0100

    day4_2

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,67 @@
 const XMAS: &str = "XMAS";
 const SAMX: &str = "SAMX";
+const MAS: &str = "MAS";
+const SAM: &str = "SAM";
 const N: usize = 140;
 
-pub fn execute(part_two: bool) -> i32 {
-    let input = include_bytes!("../input/day4.txt");
+pub fn execute(is_part_two: bool) -> i32 {
+    if is_part_two {
+        part_two()
+    } else {
+        part_one()
+    }
+}
 
+fn part_two() -> i32 {
     // transform into 140x140 grid
+    let input = include_bytes!("../input/day4.txt");
+    let mut cnt = 0;
+    let mut grid: [[u8; N]; N] = [[0; N]; N];
+    for c in input.iter() {
+        // skip newline
+        if c.is_ascii_control() {
+            continue;
+        }
+
+        // assemble grid
+        grid[cnt / N][cnt % N] = *c;
+        cnt += 1;
+    }
+
+    // search for A's
+    let mut indices = Vec::new();
+    (1..N - 1).for_each(|i| {
+        (1..N - 1).for_each(|j| {
+            let c = grid[i][j] as char;
+            if c == 'A' {
+                // now check the surrounding
+                let diag1 = format!(
+                    "{}{}{}",
+                    grid[i - 1][j - 1] as char,
+                    c,
+                    grid[i + 1][j + 1] as char
+                );
+
+                let diag2 = format!(
+                    "{}{}{}",
+                    grid[i - 1][j + 1] as char,
+                    c,
+                    grid[i + 1][j - 1] as char
+                );
+
+                if (diag1 == MAS || diag1 == SAM) && (diag2 == MAS || diag2 == SAM) {
+                    indices.push((i, j));
+                }
+            }
+        });
+    });
+
+    indices.len() as i32
+}
+
+fn part_one() -> i32 {
+    // transform into 140x140 grid
+    let input = include_bytes!("../input/day4.txt");
     let mut cnt = 0;
     let mut grid: [[u8; N]; N] = [[0; N]; N];
     for c in input.iter() {
@@ -24,10 +80,10 @@ pub fn execute(part_two: bool) -> i32 {
 
     // check grid horizontally and vertically
 
-    for i in 0..N {
+    (0..N).for_each(|i| {
         let mut hbuffer = String::new();
         let mut vbuffer = String::new();
-        for j in 0..N {
+        (0..N).for_each(|j| {
             hbuffer.push(grid[i][j] as char);
             vbuffer.push(grid[j][i] as char);
 
@@ -39,14 +95,14 @@ pub fn execute(part_two: bool) -> i32 {
             if vbuffer.ends_with(XMAS) || vbuffer.ends_with(SAMX) {
                 found_vertical += 1;
             }
-        }
-    }
+        });
+    });
 
     let mut found_diagonal = 0;
     let mut found_diagonal2 = 0;
 
     // now check it diagonally 789
-    for i in 0..N {
+    (0..N).for_each(|i| {
         // upper triangle
         {
             let mut buffer = String::new();
@@ -100,13 +156,7 @@ pub fn execute(part_two: bool) -> i32 {
                 }
             });
         }
-    }
-
-    // println!("found_horizontal: {}", found_horizontal);
-    // println!("found_vertical: {}", found_vertical);
-    // println!("found_diagonal 1: {}", found_diagonal);
-    // println!("found_diagonal 2: {}", found_diagonal2);
-    // println!("===");
+    });
 
     found_horizontal + found_vertical + found_diagonal + found_diagonal2
 }
--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ mod tests {
     #[test]
     fn test_day4() {
         assert!(day4::execute(false) == 2557);
-        //assert!(day3::execute(true) == 56275602);
+        assert!(day4::execute(true) == 1854);
     }
 }
 
@@ -40,23 +40,28 @@ mod benches {
     use super::*;
     use test::Bencher;
 
-    #[bench]
-    fn bench_day2(b: &mut Bencher) {
-        b.iter(day2::execute);
-    }
+    // #[bench]
+    // fn bench_day2(b: &mut Bencher) {
+    //     b.iter(day2::execute);
+    // }
+
+    // #[bench]
+    // fn bench_day1(b: &mut Bencher) {
+    //     b.iter(|| day1::execute(true));
+    // }
+
+    // #[bench]
+    // fn bench_day3(b: &mut Bencher) {
+    //     b.iter(|| day3::execute(true));
+    // }
 
     #[bench]
-    fn bench_day1(b: &mut Bencher) {
-        b.iter(|| day1::execute(true));
-    }
-
-    #[bench]
-    fn bench_day3(b: &mut Bencher) {
-        b.iter(|| day3::execute(true));
-    }
-
-    #[bench]
-    fn bench_day4(b: &mut Bencher) {
+    fn bench_day4p1(b: &mut Bencher) {
         b.iter(|| day4::execute(false));
+    }
+
+    #[bench]
+    fn bench_day4p2(b: &mut Bencher) {
+        b.iter(|| day4::execute(true));
     }
 }
